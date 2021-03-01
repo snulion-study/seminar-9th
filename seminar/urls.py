e39f0819a7ac51efe29d8248f6b186b47019ae58
kjw7953 <uis7953@naver.com>
2021-03-01 12:47:47 +0900

    [feat] #1 Implement Post CRD
    implement Post model's create / read / delete functions
    set static files

--- seminar/urls.py
+++ seminar/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import include
 import blogPosts.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogPosts.views.index, name='index'),
+    path('posts/', include('blogPosts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
