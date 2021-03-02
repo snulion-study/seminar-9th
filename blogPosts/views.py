0bf04e1e66d3b09cbf99ce724f191b7e3d564c73
kjw7953 <53823950+kjw7953@users.noreply.github.com>
2021-03-02 15:09:40 +0900

    Feature/#1 mtv crud (#9)
    * [create] #1 Create django project named 'seminar'
    
    * [feat] #1 Implement Post CRD
    
    implement Post model's create / read / delete functions
    set static files
    
    * [chore] #1 Change html tags and attributes
    
    add wrap attribute to textarear tag in blogPosts/new.html
    change <p> tag in blogPosts/show.html to <pre> tag
    
    * [style] #1 Edit indentation width in show.html
    
    * [chore] #1 Change css file name from 'style.css' to 'base.css'
    
    * [feat] #1 Edit return statement of '__str__' function in 'Post' model class
    
    * [style] #1 Add 'index.css' file for separation css code
    
    * [docs] #1 Add requirements.txt
    
    * [style] #1 Edit indentation in 'base.html' and 'index.html' using spaces
    
    * [style] #1 Add eof lines
    
    * [style] #1 Add eof line to '.gitignore', 'blogPosts/models.py' and 'blogPosts/views.py'
    
    * [style] #1 Change css files to scss format
    
    * [chore] #1 Change static directory structure and Add sass to '.gitignore'

---
+++ blogPosts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .models import Post
+
+# Create your views here.
+def index(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        return render(request, 'blogPosts/index.html', {'posts': posts})
+    elif request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        Post.objects.create(title=title, content=content)
+        return redirect('blogPosts:index')
+
+
+def new(request):
+    return render(request, 'blogPosts/new.html')
+
+
+def show(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'blogPosts/show.html', {'post':post})
+
+
+def delete(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    return redirect('blogPosts:index')
+
