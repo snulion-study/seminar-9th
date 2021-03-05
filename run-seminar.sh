34cdcc5fbbcb4661d57ac27bba841a414de11a92
Sangwon <born9507@snu.ac.kr>
2021-03-05 20:47:43 +0900

    [refactor] #11 Remove previous code

--- run-seminar.sh
+++ run-seminar.sh
@@ -36,24 +36,3 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
-
-
-###########################
-###### previous code ######
-
-
-#!/bin/bash
-# usage: ./run-seminar.sh {path-to-virtual-environment-folder}
-# if [ -z "$1" ] 
-# then
-#     echo "No virtual environment provided"
-# else
-#     echo "try activating virtual environment [@$1/bin/activate]"
-#     source $1/bin/activate
-# fi
-
-# pip install --upgrade pip
-# pip install -r requirements.txt 
-# python manage.py makemigrations
-# python manage.py migrate
-# python manage.py runserver
