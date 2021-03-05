9e71a3c3be63591322c7723d84b874a494894300
juho <dlwngh2113@naver.com>
2021-03-05 20:32:24 +0900

    [chore] #2 Delete static path at urls.py

--- seminar/urls.py
+++ seminar/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('posts/', include('blogPosts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', accounts.views.signup, name='signup'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
