ca365b0864cdab06eaf5a674b7a763142f86908c
Sangwon <born9507@snu.ac.kr>
2021-03-05 20:52:59 +0900

    [fix] #11 Fix bash for linux

--- run-seminar.sh
+++ run-seminar.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-# usage: ./run-seminar.sh
+# usage: './run-seminar.sh' or 'bash run-seminar.sh'
+# on linux: sed -i -e 's/\r$//' <file-name>
 
 CHECK_OS="`uname -s`"
 
 if [[ "$CHECK_OS" = "Darwin"* ]]; then # Do something under Mac OS X platform     
     echo "MAC"
     dir_name="bin"
+    python_ver=""
 elif [[ "$CHECK_OS" = "Linux"* ]]; then # Do something under GNU/Linux platform     
     echo "LINUX" 
     dir_name="bin"
+    python_ver="3"
 elif [[ "$CHECK_OS" = "MINGW32"* ]]; then # Do something under Windows NT platform     
     echo "WIN" 
     dir_name="Scripts"
+    python_ver=""
 elif [[ "$CHECK_OS" = "MINGW64"* ]]; then # Do something under Windows NT platform     
     echo "WIN" 
     dir_name="Scripts"
+    python_ver=""
 elif [[ "$CHECK_OS" = "CYGWIN"* ]]; then # Do something under Windows NT platform     
     echo "WIN"     
     dir_name="Scripts"
+    python_ver=""
 fi
 
 # try-catch
@@ -25,14 +31,20 @@ fi
     source .venv/$dir_name/activate # try to activate .venv
 } || {
     echo "No virtual environment provided"
-    python -m venv .venv # make new virtual environment
+    python$python_ver -m venv .venv # make new virtual environment
     source .venv/$dir_name/activate
 }
 echo "Activated virtual environment [.venv/$dir_name/activate]"
 
+python --version
+pip --version
+
 pip install --upgrade pip
 pip install -r requirements.txt
-# error: Microsoft Visual C++ 14.0 is required. -> https://download.microsoft.com/download/5/F/7/5F7ACAEB-8363-451F-9425-68A90F98B238/visualcppbuildtools_full.exe
+# error: Microsoft Visual C++ 14.0 is required.
+# Windows -> https://download.microsoft.com/download/5/F/7/5F7ACAEB-8363-451F-9425-68A90F98B238/visualcppbuildtools_full.exe
+# Mac -> 
+# Linux -> 
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver
