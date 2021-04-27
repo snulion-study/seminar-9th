761387b94e638ad736e1cbf9c2d762825a2975e4
Hwan <jhjung3@snu.ac.kr>
2021-04-28 02:23:42 +0900

    [refactor] Replace global variable with local variable

--- blogPosts/static/blogPosts/js/base.js
+++ blogPosts/static/blogPosts/js/base.js
@@ -1,10 +1,15 @@
-// footer의 Copyright 연도 반영
-const modifyCopyrightContent = () => {
-  const year = new Date().getFullYear();
-  document.querySelector('.copyright').textContent = `SNU LIKELION ${year}`;
-}
+(() => {
+  // footer의 Copyright 연도 반영
+  const modifyCopyrightContent = () => {
+    const year = new Date().getFullYear();
+    document.querySelector('.copyright').textContent = `SNU LIKELION ${year}`;
+  }
+  
+  modifyCopyrightContent();
+})();
 
-modifyCopyrightContent();
+
+
 
 
 
