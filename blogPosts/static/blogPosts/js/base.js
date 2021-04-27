b0b52c03bcfd2a526868c9ed524a7e03d8b39f6c
Hwan <jhjung3@snu.ac.kr>
2021-04-28 03:17:26 +0900

    [refactor] Modify code structure in base.js

--- blogPosts/static/blogPosts/js/base.js
+++ blogPosts/static/blogPosts/js/base.js
@@ -1,13 +1,7 @@
 (() => {
-  // footer의 Copyright 연도 반영
-  const modifyCopyrightContent = () => {
-    const year = new Date().getFullYear();
-    document.querySelector('.copyright').textContent = `SNU LIKELION ${year}`;
-  }
-  
-  modifyCopyrightContent();
+  const year = new Date().getFullYear();
+  document.querySelector('.copyright').textContent = `SNU LIKELION ${year}`;
 })();
-
 
 
 
