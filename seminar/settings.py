676b2bf25885d3adf8f39f7f7244ae0e62655383
yeolcook01 <ggp05412@naver.com>
2021-05-01 15:29:59 +0900

    [feat] Create django seminar project3

--- seminar/settings.py
+++ seminar/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogPosts.apps.BlogpostsConfig',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SASS_PROCESSOR_ENABLED =  True
+SASS_PROCESSOR_ROOT =  os.path.join(BASE_DIR, 'blogPosts', 'static')
