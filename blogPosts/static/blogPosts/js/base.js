ec21abe0a9dcda88eee71b2963a71c295612b1eb
Hwan <jhjung3@snu.ac.kr>
2021-05-02 04:05:11 +0900

    [feat] Add handling modal btn click function

--- blogPosts/static/blogPosts/js/base.js
+++ blogPosts/static/blogPosts/js/base.js
@@ -1,9 +1,28 @@
+import { footerFeature, logOutFeature } from './features.js';
+
+// Footer의 연도 설정
 (() => {
-  const year = new Date().getFullYear();
-  document.querySelector('.copyright').textContent = `SNU LIKELION ${year}`;
+	footerFeature.setYear();
 })();
 
+// 로그아웃 버튼 클릭 시 이벤트 핸들링
+(() => {
+	const logOutBtn = document.getElementById('log-out');
+	if (logOutBtn) {
+		logOutBtn.onclick = (e) => logOutFeature.handleLogOutBtnClick(e);
+	}
+})();
 
-
-
-
+// 로그아웃 Modal 내의 버튼 클릭 시 이벤트 핸들링
+(() => {
+	const modalLogOutBtn = document.querySelector(
+		'#log-out-modal > .modal-footer > .confirm'
+	);
+	const modalCancelBtn = document.querySelector(
+		'#log-out-modal > .modal-footer > .cancel'
+	);
+	if (modalLogOutBtn && modalCancelBtn) {
+		modalLogOutBtn.onclick = () => logOutFeature.handleModalLogOutBtnClick();
+		modalCancelBtn.onclick = () => logOutFeature.handleModalCancelBtnClick();
+	}
+})();
