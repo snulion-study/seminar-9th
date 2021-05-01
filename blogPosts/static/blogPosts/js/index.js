b0b8346ced5de2ac6577359bcc905cfe7b895fd7
Hwan <jhjung3@snu.ac.kr>
2021-05-02 04:03:52 +0900

    [refactor] Refactor index.js with module

--- blogPosts/static/blogPosts/js/index.js
+++ blogPosts/static/blogPosts/js/index.js
@@ -1,59 +1,11 @@
+import { searchFeature } from '/static/blogPosts/js/features.js';
+
+// 검색 및 리셋 기능 이벤트 핸들링
 (() => {
-  const searchBtn = document.getElementById('search-btn');
-  const searchBar = document.getElementById('search-bar');
-  const resetBtn = document.getElementById('reset-btn');
-
-  searchBtn.onclick = () => feature.search();
-  searchBar.onkeypress = (e) => feature.handleEnterPress(e);
-  resetBtn.onclick = () => feature.reset();
+	document.getElementById('search-btn').onclick = () =>
+		searchFeature.handleSearch();
+	document.getElementById('search-bar').onkeypress = (e) =>
+		searchFeature.handleEnterPress(e);
+	document.getElementById('reset-btn').onclick = () =>
+		searchFeature.handleReset();
 })();
-
-const feature = {
-  search(inputVal) {
-    const { value: searchKeyword } = document.getElementById('search-bar');
-  
-    const posts = this.getAllPosts();
-    const unMatchedPosts = this.getUnMatchedPosts(posts, inputVal ?? searchKeyword);
-
-    this.reset();
-    this.handlePostHidden(unMatchedPosts, true);
-  },
-
-  handleEnterPress(e) {
-    if (e.key === 'Enter') {
-      this.search(e.target.value);
-    }
-  },
-  
-  reset() {
-    const posts = this.getAllPosts();
-    this.handlePostHidden(posts, false);
-  },
-
-  getAllPosts() {
-    const postElements = [...document.querySelectorAll('.index-post-title')];
-
-    const posts = postElements.map(post => {
-      const [
-        { textContent: titleTextNode }, 
-        { textContent: likeCountTextNode }
-      ] = [post.firstElementChild.firstElementChild, post.lastElementChild];
-
-      return {
-        post, 
-        title: titleTextNode.trim(), 
-        likeCount: +likeCountTextNode.trim().split(/\s/)[0]
-      };
-    });
-
-    return posts;
-  },
-
-  getUnMatchedPosts(posts, searchKeyword) {
-    return posts.filter(({ title }) => !title.includes(searchKeyword));
-  },
-
-  handlePostHidden(posts, isHidden) {
-    posts.forEach(({ post }) => post.classList.toggle('hide', isHidden));
-  }
-}
