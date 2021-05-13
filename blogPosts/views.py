ae1ccac9d918fce96805042cc5d90daacac62676
sangwon <born9507@snu.ac.kr>
2021-05-13 22:12:37 +0900

    [feat] Finish Week-4 HW-advanced

--- blogPosts/views.py
+++ blogPosts/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render, redirect
 from .models import Post
+from accounts.models import Profile
+from django.db.models import Count
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     if request.method == 'GET': 
         posts = Post.objects.all()
-        return render(request, 'blogPosts/index.html', {'posts': posts})
+        colleges = Profile.objects.values('college').annotate(count=Count('college')).order_by('count')
+        users_with_same_college = None
+        users_with_same_major = None
+        
+        if request.user.is_authenticated:
+            users_with_same_college = User.objects.filter(profile__college=request.user.profile.college).exclude(id=request.user.id)
+            users_with_same_major = User.objects.filter(profile__major=request.user.profile.major).exclude(id=request.user.id)
+            
+        return render(
+            request, 
+            'blogPosts/index.html', 
+            {
+                'posts': posts, 
+                'colleges': colleges, 
+                'users_with_same_college': users_with_same_college, 
+                'users_with_same_major': users_with_same_major
+            }
+        )
+    
     elif request.method == 'POST': 
         title = request.POST['title']
         content = request.POST['content']
@@ -32,6 +53,7 @@ def update(request, id):
     if request.method == 'GET':
         post = Post.objects.get(id=id)
         return render(request, 'blogPosts/update.html', {'post':post})
+    
     elif request.method == 'POST':
         title = request.POST['title']
         content = request.POST['content']
